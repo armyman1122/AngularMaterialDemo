e401cd48afc38ea32317153ae422dfc92bf43929
Trevoris Jefferson <armyman1122@gmail.com>
2017-07-13 20:34:21 -0400

    Updated dependencies

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,9 @@
 import { TestBed, async } from '@angular/core/testing';
 
 import { AppComponent } from './app.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MdCardModule, MdDatepickerModule, MdNativeDateModule, MdTabsModule} from '@angular/material';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
@@ -8,6 +11,14 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
+      imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        MdCardModule,
+        MdDatepickerModule,
+        MdNativeDateModule,
+        MdTabsModule
+      ]
     }).compileComponents();
   }));
 
@@ -20,13 +31,13 @@ describe('AppComponent', () => {
   it(`should have as title 'app'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
+    expect(app.title).toEqual('Angular 4 Material Demo');
   }));
 
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!!');
+    expect(compiled.querySelector('h1').textContent).toContain('Angular 4 Material Demo');
   }));
 });
